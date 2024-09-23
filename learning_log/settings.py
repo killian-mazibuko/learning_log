5aa77fc1d03f4a9db6fc9a5e31fadc16d9e5c79e
Killian Mazibuko <killian.mazibuko@gmail.com>
2024-09-23 16:41:56 +0200

    Added env files

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'bootstrap5',
     # My apps
     'learning_logs',
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Topic, Entry
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    """The home page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
+
